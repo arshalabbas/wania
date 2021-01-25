cef22bbe7910a932344520725d33e30782fe30f6
Arshal Abbas <arshalabbas123@gmail.com>
2021-01-25 22:33:46 +0530

    updated invite command

--- commands/invite.js
+++ commands/invite.js
@@ -5,14 +5,19 @@ module.exports = {
   description: "Send bot invite link",
   async execute(message) {
     const invite = `https://discord.com/oauth2/authorize?client_id=${message.client.user.id}&permissions=37154112&scope=bot`;
+    const wInvite = "https://discord.com/oauth2/authorize?client_id=794170239737921536&permissions=37154112&scope=bot"
     const server = "https://discord.gg/p5MkwJVwUJ";
     const inviteEmbed = new MessageEmbed()
-    .setTitle("Invite me!")
-    .setURL(invite)
-    .addField("Supports Us",`[Invite Me](${invite})\n[Join Support Server](${server})`)
-    .setColor(16747679)
-    .setDescription("I hope I can be entertain to you in your server!")
-    .setThumbnail("https://i.ibb.co/W6hK79j/mimo-logo.png");
+      .setTitle("Invite Us!")
+      .addFields(
+        { name: "Wania Music", value: `[Click here to invite...](${invite})` },
+        { name: "Wania Radio", value: `[Click here ti invite...](${wInvite})` },
+        { name: "Support Server", value: `[Click here to join...](${server})` }
+      )
+      .setColor(16747679)
+      .setImage("https://i.ibb.co/Rj4LnHc/wania-together.gif")
+      .setDescription("I hope we can be entertain to you in your server!")
+      .setThumbnail("https://i.ibb.co/TB5ymRh/wania-radio.png");
     const emoji = emojies[Math.floor(Math.random() * emojies.length)];
     await message.react(emoji);
     return message.member
